decda2741d7dd10837d22e2baaa03633d41ce4e9
Jose <jodelabarra@alumnos.uai.cl>
2020-09-09 16:32:05 -0300

    Ayuda 3

--- 2estudia (1).py
+++ 2estudia (1).py
@@ -1,4 +1,5 @@
 from event import Estacionamiento
+import csv
 import random as ran
 
 tiempo_entre_llegadas = 5
@@ -68,13 +69,15 @@ for i in range(total_events):
 
 # report
 report = open("report.csv", "w")
+
 for time, cas, cli in zip(time, estacionamientos, clients):
     report.write('Estacionemiento 1: ' + str(time)+","+str(cas)+","+str(cli)+"\n")
 report.close()
 
 
 # report 2
-# report = open("report_2.csv", "w")
-# for time_2, cas_2, cli_2 in zip(time, estacionamientos_2, clients):
-#    report.write('Estacionemiento 2: ' + str(time_2)+","+str(cas_2)+","+str(cli_2)+"\n")
-# report.close()
+
+report_2 = open("report_2.csv", "w")
+for time, cas_2, cli in zip(time, estacionamientos_2, clients):
+    report_2.write('Estacionemiento 2: ' + str(time)+","+str(cas_2)+","+str(cli)+"\n")
+ report_2.close()
